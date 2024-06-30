d2df99f875a517b4e0c07760b34048f98c5b9db2
yeon2lee <md04254@naver.com>
2024-06-30 20:19:00 +0900

    test: 결과값 테스트 추가

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -30,5 +30,33 @@ class ApplicationTest {
         // ByteArrayOutputStream의 toString은 기본 문자집합을 사용하여 버퍼의 내용을 문자열 디코딩 바이트로 변환해줍니다.
         return outputStreamCaptor.toString();
     }
-    
+
+    @Test
+    public void 우승자_발표() throws Exception {
+        List<Car> cars = Arrays.asList(
+                new Car("pobi", 6),
+                new Car("crong", 3),
+                new Car("honux", 1)
+        );
+        Racing racing = new Racing(cars, 0);
+        racing.race();
+        ResultView.printWinners(racing.getWinners());
+
+        assertThat(getOutput()).contains("pobi : ------", "pobi가 최종 우승했습니다.");
+    }
+
+    @Test
+    public void 공동_우승자_발표() throws Exception {
+        List<Car> cars = Arrays.asList(
+                new Car("pobi", 6),
+                new Car("crong", 6),
+                new Car("honux", 1)
+        );
+        Racing racing = new Racing(cars, 0);
+        racing.race();
+        ResultView.printWinners(racing.getWinners());
+
+        assertThat(getOutput()).contains("pobi : ------", "crong : ------", "pobi, crong가 최종 우승했습니다.");
+    }
+
 }
