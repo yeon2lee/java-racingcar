47d5c75ab6e793543adcf40df01e875324b3d044
yeon2lee <md04254@naver.com>
2024-06-30 19:19:37 +0900

    refactor: 접근 제한자 private 사용

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -24,7 +24,7 @@ public class Racing {
         return winners;
     }
 
-    public int getRandomNo() {
+    private int getRandomNo() {
         final Random random = new Random();
         return random.nextInt(10);
     }
